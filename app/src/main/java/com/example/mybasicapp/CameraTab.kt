6aa35edad02b8706d6f96c1671b9cf43935d8b10
Ahmad Cooper <108217835+AhmadAC@users.noreply.github.com>
2026-07-11 09:31:18 +0800

    ss

--- app/src/main/java/com/example/mybasicapp/CameraTab.kt
+++ app/src/main/java/com/example/mybasicapp/CameraTab.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
